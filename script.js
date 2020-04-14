a48555152b0f549726b8e1e2241098143ac30010
Priscilla <mycilla74@gmail.com>
2020-04-14 18:08:13 +0100

    created javascript file and used jquery

---
+++ script.js
@@ -0,0 +1,141 @@
+$(document).ready(() => {
+	$(".image1").on("mouseenter", () => {
+		$(".image1 p").css("background-color", "yellow");
+	})
+	$(".image1").on("mouseleave", () => {
+		$(".image1 p").css("background-color", "white");
+	})
+	$(".image2").on("mouseenter", () => {
+		$(".image2 p").css("background-color", "yellow");
+	})
+	$(".image2").on("mouseleave", () => {
+		$(".image2 p").css("background-color", "white");
+	})
+	$(".image3").on("mouseenter", () => {
+		$(".image3 p").css("background-color", "yellow");
+	})
+	$(".image3").on("mouseleave", () => {
+		$(".image3 p").css("background-color", "white");
+	})
+
+	$(".icon1").on("mouseenter", () => {
+		$(".icon1 p").css("background-color", "yellow");
+	})
+	$(".icon1").on("mouseleave", () => {
+		$(".icon1 p").css("background-color", "white");
+	})
+	$(".icon2").on("mouseenter", () => {
+		$(".icon2 p").css("background-color", "yellow");
+	})
+	$(".icon2").on("mouseleave", () => {
+		$(".icon2 p").css("background-color", "white");
+	})
+	$(".icon3").on("mouseenter", () => {
+		$(".icon3 p").css("background-color", "yellow");
+	})
+	$(".icon3").on("mouseleave", () => {
+		$(".icon3 p").css("background-color", "white");
+	})
+	$(".icon4").on("mouseenter", () => {
+		$(".icon4 p").css("background-color", "yellow");
+	})
+	$(".icon4").on("mouseleave", () => {
+		$(".icon4 p").css("background-color", "white");
+	})
+	$(".icon5").on("mouseenter", () => {
+		$(".icon5 p").css("background-color", "yellow");
+	})
+	$(".icon5").on("mouseleave", () => {
+		$(".icon5 p").css("background-color", "white");
+	})
+	$(".icon6").on("mouseenter", () => {
+		$(".icon6 p").css("background-color", "yellow");
+	})
+	$(".icon6").on("mouseleave", () => {
+		$(".icon6 p").css("background-color", "white");
+	})
+	$(".icon7").on("mouseenter", () => {
+		$(".icon7 p").css("background-color", "yellow");
+	})
+	$(".icon7").on("mouseleave", () => {
+		$(".icon7 p").css("background-color", "white");
+	})
+	$(".icon8").on("mouseenter", () => {
+		$(".icon8 p").css("background-color", "yellow");
+	})
+	$(".icon8").on("mouseleave", () => {
+		$(".icon8 p").css("background-color", "white");
+	})
+	$(".icon9").on("mouseenter", () => {
+		$(".icon9 p").css("background-color", "yellow");
+	})
+	$(".icon9").on("mouseleave", () => {
+		$(".icon9 p").css("background-color", "white");
+	})
+	$(".icon10").on("mouseenter", () => {
+		$(".icon10 p").css("background-color", "yellow");
+	})
+	$(".icon10").on("mouseleave", () => {
+		$(".icon10 p").css("background-color", "white");
+	})
+	$(".icon11").on("mouseenter", () => {
+		$(".icon11 p").css("background-color", "yellow");
+	})
+	$(".icon11").on("mouseleave", () => {
+		$(".icon11 p").css("background-color", "white");
+	})
+	$(".icon12").on("mouseenter", () => {
+		$(".icon12 p").css("background-color", "yellow");
+	})
+	$(".icon12").on("mouseleave", () => {
+		$(".icon12 p").css("background-color", "white");
+	})
+	$(".icon13").on("mouseenter", () => {
+		$(".icon13 p").css("background-color", "yellow");
+	})
+	$(".icon13").on("mouseleave", () => {
+		$(".icon13 p").css("background-color", "white");
+	})
+	$(".icon14").on("mouseenter", () => {
+		$(".icon14 p").css("background-color", "yellow");
+	})
+	$(".icon14").on("mouseleave", () => {
+		$(".icon14 p").css("background-color", "white");
+	})
+	$(".icon15").on("mouseenter", () => {
+		$(".icon15 p").css("background-color", "yellow");
+	})
+	$(".icon15").on("mouseleave", () => {
+		$(".icon15 p").css("background-color", "white");
+	})
+	$(".icon16").on("mouseenter", () => {
+		$(".icon16 p").css("background-color", "yellow");
+	})
+	$(".icon16").on("mouseleave", () => {
+		$(".icon16 p").css("background-color", "white");
+	})
+	$(".icon17").on("mouseenter", () => {
+		$(".icon17 p").css("background-color", "yellow");
+	})
+	$(".icon17").on("mouseleave", () => {
+		$(".icon17 p").css("background-color", "white");
+	})
+	$(".icon18").on("mouseenter", () => {
+		$(".icon18 p").css("background-color", "yellow");
+	})
+	$(".icon18").on("mouseleave", () => {
+		$(".icon18 p").css("background-color", "white");
+	})
+	$(".icon19").on("mouseenter", () => {
+		$(".icon19 p").css("background-color", "yellow");
+	})
+	$(".icon19").on("mouseleave", () => {
+		$(".icon19 p").css("background-color", "white");
+	})
+	$(".icon20").on("mouseenter", () => {
+		$(".icon20 p").css("background-color", "yellow");
+	})
+	$(".icon20").on("mouseleave", () => {
+		$(".icon20 p").css("background-color", "white");
+	})
+});
